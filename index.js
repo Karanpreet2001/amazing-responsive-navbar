341e573ceea4d6265e17e1e82d53730eabfdeaa2
Karanpreet <karanpreetfarwaha@gmail.com>
2022-08-29 20:55:49 -0700

    adding javascript for toggling

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const menuButton = document.querySelector(".mobile-navbar-button");
+const header = document.querySelector(".header");
 
 
 
 const toggleNavBar = () =>{
-        
+        header.classList.toggle('active');
 }
 
 menuButton.addEventListener('click', toggleNavBar);
